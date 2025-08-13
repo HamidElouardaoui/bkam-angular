51f97e09ea15661274969314108d89309b067d31
HamidMobiArch <hamid.e@mobiarchitects.com>
2025-08-13 12:19:09 +0100

    MàJ

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { Header } from "../components/header/header";
+import { CommonModule } from '@angular/common';
+import { Table } from "../components/table/table";
+export interface RequestRow {
+  company: string;
+  type: string;
+  createdAt: string; // already formatted text
+  status: 'Valide' | 'Retourné' | 'En cours';
+}
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, CommonModule, Table],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
   protected readonly title = signal('bkam-angular');
+
 }
---
+++ src/components/table/table.ts
@@ -0,0 +1,39 @@
+import { Component, Input } from '@angular/core';
+import { RequestRow } from '../../app/app';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-table',
+  imports: [CommonModule],
+  templateUrl: './table.html',
+  styleUrl: './table.scss'
+})
+export class Table {
+ @Input() rows: RequestRow[] = [
+    { company: 'AssurNova & Trousseau', type: 'Changement de Logo/Adresse', createdAt: '07-08-2025 15:26', status: 'Retourné' },
+    { company: 'Général CORPORATE',      type: 'Changement de Logo/Adresse', createdAt: '29-07-2025 17:12', status: 'Valide' },
+    { company: 'Général CORPORATE',      type: "Changement d’actionnaire",    createdAt: '29-07-2025 14:57', status: 'Valide' },
+    { company: 'AssurNova & Trousseau',  type: 'Changement de gouvernance',   createdAt: '28-07-2025 10:07', status: 'Valide' },
+  ];
+
+  pillClasses(s: RequestRow['status']) {
+    switch (s) {
+      case 'Valide':
+        return 'bg-green-100 text-green-700';
+      case 'Retourné':
+        return 'bg-orange-100 text-orange-700';
+      default:
+        return 'bg-zinc-100 text-zinc-700';
+    }
+  }
+  dotClasses(s: RequestRow['status']) {
+    switch (s) {
+      case 'Valide':
+        return 'bg-green-500';
+      case 'Retourné':
+        return 'bg-orange-500';
+      default:
+        return 'bg-zinc-400';
+    }
+  }
+}
